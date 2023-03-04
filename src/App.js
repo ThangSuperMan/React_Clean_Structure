70b8904e821fdd67bc0d2bb2b411cbb33c9013d0
thangsuperman <thangjenny2002@gmail.com>
2023-03-04 22:44:36 +0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Feed from "./pages/Feed";
 import Layout from "./components/Layout";
 import routes from "./pages/routes";
 import "./App.css";
 
 console.log("routes :>> ", routes);
 
-// console.log("Layout component :>> ", Layout);
-
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          {routes.map(({ component: Component, path, ...rest }) => {
-            console.log("component :>> ", Component);
-            console.log("path :>> ", path);
-            return (
-              <Route
-                // element={`<${component} />`}
-                element={Component}
-                path={path}
-                key={path}
-              />
-            );
+          {routes.map(({ component, path }) => {
+            return <Route element={component} path={path} key={path} />;
           })}
-          // <Route index element={<Home />} />
-          // <Route path="/feed" element={<Feed />} />
         </Routes>
       </Layout>
     </BrowserRouter>
